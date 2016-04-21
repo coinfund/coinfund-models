33309c78ee7f5aee6ea08116741373303f0c6271
Jake Brukhman <jbrukh@gmail.com>
2016-04-21 00:30:27 -0400

    Secure password for user.

--- app/models/coinfund/user.rb
+++ app/models/coinfund/user.rb
@@ -1,4 +1,19 @@
 module Coinfund
   class User < ApplicationRecord
+    include ActiveModel::SecurePassword
+    
+    # User has a secure password.
+    has_secure_password
+
+    # User's access token is created on User create.
+    before_create :renew_access_token
+  
+    private
+      
+      # Set the access token for a user.
+      def renew_access_token
+        self.access_token = SecureRandom.hex
+      end
+      
   end
 end
