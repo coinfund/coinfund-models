5a94a85cd952029ea0a0780cd8a3f2bdc5cf16e1
Jake Brukhman <jbrukh@gmail.com>
2016-04-21 01:01:57 -0400

    Basic user tests.

--- app/models/coinfund/user.rb
+++ app/models/coinfund/user.rb
@@ -19,8 +19,6 @@ module Coinfund
                 uniqueness: { case_sensitive: false }, 
                 length: { maximum: 255 }, 
                 format: { with: VALID_EMAIL_REGEX }
-    validates :password, presence: true
-
   
     private
       
--- test/models/coinfund/user_test.rb
+++ test/models/coinfund/user_test.rb
@@ -2,8 +2,39 @@ require 'test_helper'
 
 module Coinfund
   class UserTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    
+    setup :setup_user
+
+    test "user is valid" do
+      assert @user.valid?, @user.errors.full_messages
+    end
+
+    test "user must have a first_name" do
+      @user.first_name = nil
+      assert_not @user.valid?
+    end
+
+    test "user must have a last_name" do
+      @user.last_name = nil
+      assert_not @user.valid?
+    end
+
+    test "user must have an email" do
+      @user.email = nil
+      assert_not @user.valid?
+    end
+
+    test "user must have a unique email" do
+      clone = coinfund_users(:jake_clone)
+      clone.email = 'jake@coinfund.io'
+      assert_not clone.valid?
+    end
+
+
+    private
+
+      def setup_user
+        @user = coinfund_users(:jake_clone)
+      end
   end
 end
